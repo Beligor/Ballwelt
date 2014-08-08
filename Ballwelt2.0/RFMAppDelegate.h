7485d2c44fa60d37999767e221827b9999144039
rafa <rafavpb@gmail.com>
2014-08-08 17:04:33 +0200

    Initial Commit

---
+++ Ballwelt2.0/RFMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RFMAppDelegate.h
+//  Ballwelt2.0
+//
+//  Created by Rafa Ferrero on 08/08/14.
+//  Copyright (c) 2014 Rafa Ferrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ballwelt2.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ballwelt2.0
+//
+//  Created by Rafa Ferrero on 08/08/14.
+//  Copyright (c) 2014 Rafa Ferrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RFMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RFMAppDelegate class]));
+    }
+}
